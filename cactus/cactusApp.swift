45de6afadaa1f2d3f31bad5e23ab000d4431fc6e
Simon <simi1232155@gmail.com>
2026-06-01 12:05:26 +0200

    Initial Commit

---
+++ cactus/cactusApp.swift
@@ -0,0 +1,17 @@
+//
+//  cactusApp.swift
+//  cactus
+//
+//  Created by Simon Gabryel on 01.06.26.
+//
+
+import SwiftUI
+
+@main
+struct cactusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
